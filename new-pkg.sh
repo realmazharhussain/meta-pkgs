7f4472a12fc8cf3a26bccf598b6896bb441c5baf
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-11-28 23:45:21 +0500

    see for yourself

---
+++ new-pkg.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+scriptfile=$(realpath "$0")
+scriptdir=$(dirname "$scriptfile")
+pkgname="$1"
+if [ ! "$pkgname" ]; then
+   echo "Error: No package name provided!" >&2
+   exit 1
+fi
+
+pkgdir="$scriptdir/PKGBUILDs/$pkgname"
+if [ -d "$pkgdir" ]; then
+   echo "Package already exists!" >&2
+   exit 2
+fi
+
+if [[ $pkgname == *-meta ]]; then
+   _pkgname=${pkgname%-meta}
+fi
+
+install="post_install() {
+}
+pre_remove() {
+}"
+
+PKGBUILD="# Maintainer: Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
+pkgname=$pkgname
+pkgver=1
+pkgrel=1
+pkgdesc=\"Meta Package for ${_pkgname:-$pkgname}\"
+arch=(any)
+url=\"https://github.com/realmazharhussain/meta-pkgs/PKGBUILDs/\$pkgname\"
+license=('GPL')
+depends=(${_pkgname})"
+
+mkdir -p "$pkgdir"
+echo "$PKGBUILD" > "$pkgdir/PKGBUILD"
+echo "$install" > "$pkgdir/$pkgname.install"
