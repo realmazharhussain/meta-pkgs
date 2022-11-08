e04d4d39dc53f8d9537f63079e5f6eae5dc6b8ae
Mazhar Hussain <realmazharhussain@gmail.com>
2022-11-08 15:36:48 +0500

    Enable signing in ./build.sh and ./update-repo.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 script_file=$(realpath "$0")
 script_dir=$(dirname "${script_file}")
+gpg_key_id=D4A940F2A8CAABCDDBEBFBFDC2C4EBF5813AB6E3
 
 rm "$script_dir"/repo/*.pkg.tar.zst
+rm "$script_dir"/repo/*.sig
 
 for pkg_dir in "${script_dir}"/PKGBUILDs/*; do
    cd "$pkg_dir"
-   makepkg -cd
-   mv "$pkg_dir"/*.pkg.tar.zst "$script_dir"/repo/
+   makepkg -cd --sign --key "${gpg_key_id}"
+   mv "$pkg_dir"/*.pkg.tar.zst{,.sig} "$script_dir"/repo/
 done
--- update-repo.sh
+++ update-repo.sh
@@ -6,9 +6,14 @@ repo_dir="$script_dir/repo"
 compression_ext='tar.xz'
 db="${repo_dir}/${repo_name}".db
 files="${repo_dir}/${repo_name}".files
+gpg_key_id=D4A940F2A8CAABCDDBEBFBFDC2C4EBF5813AB6E3
 
-repo-add "${db}.${compression_ext}" "${script_dir}"/repo/*.pkg.tar.zst
+repo-add --sign --key "${gpg_key_id}" "${db}.${compression_ext}" "${script_dir}"/repo/*.pkg.tar.zst
 
-rm "${db}" "${files}"
+rm "${db}" "${db}".sig
 mv "${db}.${compression_ext}" "${db}"
+mv "${db}.${compression_ext}".sig "${db}".sig
+
+rm "${files}" "${files}".sig
 mv "${files}.${compression_ext}" "${files}"
+mv "${files}.${compression_ext}".sig "${files}".sig
